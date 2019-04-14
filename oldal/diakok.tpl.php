7aa6ecbd0dc08f6cff10a3f829ff81b4f5b47d70
Gyurovszki Péter <gyurovszki.peter@gmail.com>
2019-04-14 11:11:08 +0200

    Update diakok.tpl.php

--- oldal/diakok.tpl.php
+++ oldal/diakok.tpl.php
@@ -1,12 +1,8 @@
-<html>
-<head>
-</head>
-
 <body>
-<div class="container" id="main-content">
-<h1 >Osztályok a 2018/2019-es tanévben</h1>
+<div align="center" class="container" id="main-content">
+<h4 >Osztályok a 2018/2019-es tanévben</h4>
 
-<h4>1.Osztály</h4>	 
+<h5>1.Osztály</h5>	 
 Laurinyecz Letti<br>
 Medvegy Áron<br>
 Omiljak Barnabás<br>
@@ -14,7 +10,7 @@ Rácz Botond<br>
 Sebestyén Benedek<br>
 Szász Mária<br>
 Tar Benjamin<br>
-<h4>2.Osztály</h4>	
+<h5>2.Osztály</h5>	
 Laurinyecz Emma<br>
 Ancsin Péter<br>
 Szász Dávid<br>
@@ -29,7 +25,7 @@ Szántai Tímea<br>
 Csák Norbert Máté<br>
 Kovács Ákos János<br>
 
-<h4>3.Osztály</h4>	
+<h5>3.Osztály</h5>	
 Kotroczó Dóra<br>
 Kovács Kornél<br>
 Krajcsó Dávid<br>
@@ -40,7 +36,7 @@ Molcsán Szabolcs István<br>
 Rácz Viktor<br>
 Szántai Orsolya<br>
 Szegedi Eszter<br>
-<h4>4.Osztály</h4>	
+<h5>4.Osztály</h5>	
 Ancsin-Rodák Ádám<br>
 Bakó-Kapros Daniella<br>
 Faragó Noémi Kinga<br>
@@ -59,7 +55,7 @@ Szász Roland<br>
 Szenczi Szonja<br>
 Tóth Renáta<br>
 
-<h4>5.Osztály</h4>	
+<h5>5.Osztály</h5>	
 Bánfi Róbert<br>
 Borbély Gabriella<br>
 Borka Noémi<br>
@@ -73,7 +69,7 @@ Máté Anita Julianna<br>
 Petrovszki Anna<br>
 Petrovszki Rita<br>
 Szabó Dorina<br>
-<h4>6.Osztály</h4>	
+<h5>6.Osztály</h5>	
 Csicsely Tünde Zita<br>
 Hódos Zsolt<br>
 Leszkó Bence Máté<br>
@@ -83,7 +79,7 @@ Petrovszki Áron<br>
 Petrovszki Hanna<br>
 Viczián Vanessza Brigitta<br>
 
-<h4>7.Osztály</h4>	
+<h5>7.Osztály</h5>	
 Bencsik Andor<br>
 Duna Alexandra<br>
 Guba Pál János<br>
@@ -94,14 +90,12 @@ Laurinyecz Eliza<br>
 Máté Attila<br>
 Megyeri Gábor<br>
 Vidovenyecz Emese Jázmin<br>
-<h4>8.Osztály</h4>	
+<h5>8.Osztály</h5>	
 Ancsin Balázs<br>
 Bárdon Csilla<br>
 Kossuth Hugó Ádám<br>
 Péter Cintia<br>
 Szabó Adél<br>
 Viczián Martin<br>
-
 </div>
 </body>
-</html>
